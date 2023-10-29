77aae4a348579d6012e214176cb2772a8b1986c8
Prakhar Jain <prakhar.math.jain@gmail.com>
2023-10-29 05:51:35 +0530

    added radio buttons actual options are to be added and name ids labels and for is to be dealt with

--- psychometric-analyser/src/MathPuzzle.js
+++ psychometric-analyser/src/MathPuzzle.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class MathPuzzle extends Component {
     render() {
@@ -11,54 +11,122 @@ export default class MathPuzzle extends Component {
                     <h5 className='fw-light mt-5'>
                         Q1. Binary Conversion - Convert a binary number to decimal:
                         <br />
-                        &emsp;&ensp;&nbsp;Puzzle : Convert "1101" to decimal.
+                        &emsp;&ensp;&nbsp;Puzzle: Convert "1101" to decimal.
                     </h5>
-                    <div className="form">
-                        <textarea className="form-control" id="form4Example3" rows="4" style={{ "resize": "None" }}></textarea>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault1" id="flexRadioDefault1_1" />
+                        <label className="form-check-label" for="flexRadioDefault1_1">Option 1</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault1" id="flexRadioDefault1_2" />
+                        <label className="form-check-label" for="flexRadioDefault1_2">Option 2</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault1" id="flexRadioDefault1_3" />
+                        <label className="form-check-label" for="flexRadioDefault1_3">Option 3</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault1" id="flexRadioDefault1_4" />
+                        <label className="form-check-label" for="flexRadioDefault1_4">Option 4</label>
                     </div>
                 </div>
 
                 <div>
-                    <h5 className='fw-light mt-5'>Q2. Complex Algebra: Solve for x in a complex algebraic equation:
+                    <h5 className='fw-light mt-5'>
+                        Q2. Complex Algebra: Solve for x in a complex algebraic equation:
                         <br />
-                        &emsp;&ensp;&nbsp;Puzzle: Solve for x in the equation: 2x^2 - 5x + 3 = 0.</h5>
-                    <div className="form mb-4">
-                        <textarea className="form-control" id="form4Example3" rows="4" style={{ "resize": "None" }}></textarea>
+                        &emsp;&ensp;&nbsp;Puzzle: Solve for x in the equation: 2x^2 - 5x + 3 = 0.
+                    </h5>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault2" id="flexRadioDefault2_1" />
+                        <label className="form-check-label" for="flexRadioDefault2_1">Option 1</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault2" id="flexRadioDefault2_2" />
+                        <label className="form-check-label" for="flexRadioDefault2_2">Option 2</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault2" id="flexRadioDefault2_3" />
+                        <label className="form-check-label" for="flexRadioDefault2_3">Option 3</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault2" id="flexRadioDefault2_4" />
+                        <label className="form-check-label" for="flexRadioDefault2_4">Option 4</label>
                     </div>
                 </div>
 
                 <div>
-                    <h5 className='fw-light mt-5'>Q3. Prime Factorization: Find the prime factors of a large number:
+                    <h5 className='fw-light mt-5'>
+                        Q3. Prime Factorization: Find the prime factors of a large number:
                         <br />
-                        &emsp;&ensp;&nbsp;Puzzle: Find the prime factors of 168.</h5>
-                    <div className="form mb-4">
-                        <textarea className="form-control" id="form4Example3" rows="4" style={{ "resize": "None" }}></textarea>
+                        &emsp;&ensp;&nbsp;Puzzle: Find the prime factors of 168.
+                    </h5>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault3" id="flexRadioDefault3_1" />
+                        <label className="form-check-label" for="flexRadioDefault3_1">Option 1</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault3" id="flexRadioDefault3_2" />
+                        <label className="form-check-label" for="flexRadioDefault3_2">Option 2</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault3" id="flexRadioDefault3_3" />
+                        <label className="form-check-label" for="flexRadioDefault3_3">Option 3</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault3" id="flexRadioDefault3_4" />
+                        <label className="form-check-label" for="flexRadioDefault3_4">Option 4</label>
                     </div>
                 </div>
-
 
                 <div>
-                    <h5 className='fw-light mt-5'>Q4. Calculus Challenge: Calculate the derivative of a complex function:
+                    <h5 className='fw-light mt-5'>
+                        Q4. Calculus Challenge: Calculate the derivative of a complex function:
                         <br />
-                        &emsp;&ensp;&nbsp;Puzzle: Find the derivative of f(x) = 3x^4 - 6x^3 + 2x^2 - 8x + 1.</h5>
-                    <div className="form mb-4">
-                        <textarea className="form-control" id="form4Example3" rows="4" style={{ "resize": "None" }}></textarea>
+                        &emsp;&ensp;&nbsp;Puzzle: Find the derivative of f(x) = 3x^4 - 6x^3 + 2x^2 - 8x + 1.
+                    </h5>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault4" id="flexRadioDefault4_1" />
+                        <label className="form-check-label" for="flexRadioDefault4_1">Option 1</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault4" id="flexRadioDefault4_2" />
+                        <label className="form-check-label" for="flexRadioDefault4_2">Option 2</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault4" id="flexRadioDefault4_3" />
+                        <label className="form-check-label" for="flexRadioDefault4_3">Option 3</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault4" id="flexRadioDefault4_4" />
+                        <label className="form-check-label" for="flexRadioDefault4_4">Option 4</label>
                     </div>
                 </div>
-
 
                 <div>
-                    <h5 className='fw-light mt-5'>Q5. Number Base Conversion: Convert a number from one base to another.
+                    <h5 className='fw-light mt-5'>
+                        Q5. Number Base Conversion: Convert a number from one base to another.
                         <br />
-                        &emsp;&ensp;&nbsp;Puzzle: Convert the number "101011" from binary to its decimal equivalent.</h5>
-                    <div className="form mb-4">
-                        <textarea className="form-control" id="form4Example3" rows="4" style={{ "resize": "None" }}></textarea>
+                        &emsp;&ensp;&nbsp;Puzzle: Convert the number "101011" from binary to its decimal equivalent.
+                    </h5>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault5" id="flexRadioDefault5_1" />
+                        <label className="form-check-label" for="flexRadioDefault5_1">Option 1</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault5" id="flexRadioDefault5_2" />
+                        <label className="form-check-label" for="flexRadioDefault5_2">Option 2</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault5" id="flexRadioDefault5_3" />
+                        <label className="form-check-label" for="flexRadioDefault5_3">Option 3</label>
+                    </div>
+                    <div>
+                        <input type="radio" className="form-check-input" name="flexRadioDefault5" id="flexRadioDefault5_4" />
+                        <label className="form-check-label" for="flexRadioDefault5_4">Option 4</label>
                     </div>
                 </div>
-
-
-
             </div>
-        )
+        );
     }
 }
